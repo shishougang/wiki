eea7155f4b02e440fb59b71bf9760cb8dc326cb7
SHI Shougang <shishougang@gmail.com>
2012-07-12 00:20:00 +0800

    initial

---
+++ src/notes-init.el
@@ -0,0 +1,38 @@
+(require 'org-publish)
+;; Modified From meteor1113@newsmth
+;; URL: http://blog.csdn.net/meteor1113/archive/2009/07/30/4395673.aspx
+;; http://orgmode.org/worg/org-tutorials/org-publish-html-tutorial.html
+
+(setq note-root-dir
+      (file-name-directory (or load-file-name (buffer-file-name))))
+
+(setq note-publish-dir
+      (expand-file-name "public_html" (directory-file-name
+                                       (file-name-directory
+                                        (directory-file-name note-root-dir)))))
+
+(setq org-publish-project-alist
+      `(("wiki-org"
+         :base-directory ,note-root-dir
+         :publishing-directory ,note-publish-dir
+         :base-extension "org"
+         :style "<link rel=\"stylesheet\" href=\"./assets/stylesheet.css\" type=\"text/css\"/>"
+         :recursive t
+         :publishing-function org-publish-org-to-html
+         :auto-index t
+;         :index-filename "index.org"
+;         :index-title "index"
+         :auto-sitemap t                ; Generate sitemap.org automagically...
+         :sitemap-filename "index.org"  ; ... call it sitemap.org (it's the default)...
+         :sitemap-title "index"         ; ... with title 'sitemap'.
+         :link-home "index.html")
+        ("wiki-static"
+         :base-directory ,note-root-dir
+         :publishing-directory ,note-publish-dir
+         :recursive t
+         :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|swf\\|zip\\|gz\\|txt\\|el"
+         :publishing-function org-publish-attachment)
+        ("wiki" 
+         :components ("wiki-org" "wiki-static")
+         :author "shougangshi@gmail.com")
+        ))
