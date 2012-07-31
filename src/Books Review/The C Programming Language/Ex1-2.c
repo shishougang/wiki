9758062f27600fe29f2eb0946fac07ad178e813d
SHI Shougang <shishougang@gmail.com>
2012-07-31 12:06:37 +0800

    update

---
+++ src/Books Review/The C Programming Language/Ex1-2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+  printf("声音或视觉警告。 \a\n");  
+  printf("换页. \f\n");
+  printf("This escape, \r, 光标移到本行的起始位置\n");
+  printf("纵向制表 \v 。。。\n");
+
+  return 0;
+}
---
+++ upload.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+git add .
+git commit -am "update"
+git push
