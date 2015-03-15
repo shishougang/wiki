09a77852209398c1d91670b70ecdd269fc723ea5
SHI Shougang <shishougang@gmail.com>
2015-03-15 18:18:52 +0800

    update

--- public_html/notes-init.el
+++ public_html/notes-init.el
@@ -11,8 +11,11 @@
                                        (file-name-directory
                                         (directory-file-name note-root-dir)))))
 
-(setq org-publish-project-alist
-      `(("wiki-org"
+(unless (boundp 'org-publish-project-alist)
+  (setq org-publish-project-alist nil))
+
+(add-to-list 'org-publish-project-alist
+      `("wiki-org"
          :base-directory ,note-root-dir
          :publishing-directory ,note-publish-dir
          :base-extension "org"
@@ -25,15 +28,18 @@
          :auto-sitemap t                ; Generate sitemap.org automagically...
          :sitemap-filename "index.org"  ; ... call it sitemap.org (it's the default)...
          :sitemap-title "index"         ; ... with title 'sitemap'.
-         :link-home "index.html")
-        ("wiki-static"
+         :link-home "index.html"))
+
+(add-to-list 'org-publish-project-alist
+        `("wiki-static"
          :base-directory ,note-root-dir
          :publishing-directory ,note-publish-dir
          :recursive t
          :base-extension "css\\|js\\|png\\|jpg\\|jpeg\\|svg\\|gif\\|pdf\\|mp3\\|swf\\|zip\\|gz\\|txt\\|el\\|tar.gz\\|c\\|cpp\\|sh"
-         :publishing-function org-publish-attachment)
+         :publishing-function org-publish-attachment))
 
-        ("octopress" 
+(add-to-list 'org-publish-project-alist
+      `("octopress" 
           :base-directory "~/Dropbox/Blog/octopress/source/org_posts/"
           :base-extension "org"
           :publishing-directory "~/Dropbox/Blog/octopress/source/_posts/"
@@ -42,9 +48,9 @@
           :publishing-function org-html-publish-to-html 
           :headline-levels 4
           :html-extension "markdown"
-          :body-only t)
+          :body-only t))
 
-        ("wiki" 
+(add-to-list 'org-publish-project-alist
+      `("wiki" 
          :components ("wiki-org" "wiki-static")
-         :author "shougangshi@gmail.com")
-        ))
+         :author "shougangshi@gmail.com"))
