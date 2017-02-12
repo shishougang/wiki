387b0ab04e67d1f46c49327cfc6b4a9b0bc7ed36
SHI Shougang <shishougang@gmail.com>
2017-02-13 00:36:55 +0800

    update

--- public_html/Misc/Software/Files/rtorrent.sh
+++ public_html/Misc/Software/Files/rtorrent.sh
@@ -3,22 +3,22 @@
 case "$1" in
   start)
         echo -n "Starting rtorrent"
-        su - root -c "screen -A -m -d -S rtorrent /usr/local/bin/rtorrent" &
+        su - root -c "screen -A -m -d -S rtorrent /usr/bin/rtorrent" &
         echo "."
         ;;
   stop)
         echo -n "Stopping rtorrent"
-        ppid=`ps ax | grep "/usr/local/bin/rtorrent" | grep -v grep | grep -v screen | awk '{ print $1 }'`
+        ppid=`ps ax | grep "/usr/bin/rtorrent" | grep -v grep | grep -v screen | awk '{ print $1 }'`
         kill ${ppid}
         echo "."
         ;;
   restart)
 
         echo -n "Restarting rtorrent"
-        ppid=`ps ax | grep "/usr/local/bin/rtorrent" | grep -v grep | grep -v screen | awk '{ print $1 }'`
+        ppid=`ps ax | grep "/usr/bin/rtorrent" | grep -v grep | grep -v screen | awk '{ print $1 }'`
         kill ${ppid}
         sleep 1
-        su - root -c "screen -A -m -d -S rtorrent /usr/local/bin/rtorrent" &
+        su - root -c "screen -A -m -d -S rtorrent /usr/bin/rtorrent" &
         echo "."
         ;;
   *)
